71bb86a48b740569daea9188220d5251b56cfa66
Richard Fry <Rich@richfry.com>
2019-02-03 10:58:05 -0700

    End of Video

--- GraniteWarehouse/Data/ApplicationDbContext.cs
+++ GraniteWarehouse/Data/ApplicationDbContext.cs
@@ -14,6 +14,7 @@ namespace GraniteWarehouse.Data
         {
         }
 
-        DbSet<ProductTypes> ProductTypes { get; set; }
+        public DbSet<ProductTypes> ProductTypes { get; set; }
+        public DbSet<SpecialTags> SpecialTags { get; set; }
     }
 }
